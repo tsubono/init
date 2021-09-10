5a4ae17543e309274fa5b5455a79b06473a8c07d
kawamoto <bahihzss@gmail.com>
2021-09-10 23:36:26 +0900

    TikTokのPlayerをTikTokMovieを使った仕様に変更

--- resources/js/components/MoviePlayer/players/TikTokPlayer.vue
+++ resources/js/components/MoviePlayer/players/TikTokPlayer.vue
@@ -2,13 +2,15 @@
     <div class="TikTokPlayer">
         <iframe
             name="__tt_embed__v39952534415998376"
-            :src="embedUrl"
+            :src="movie.playerUrl"
             style="width: 100%; height: 887px; display: block; visibility: unset; max-height: 887px;"
         ></iframe>
     </div>
 </template>
 
 <script>
+import { TikTokMovie } from '../models/TikTokMovie'
+
 export default {
     name: 'TikTokPlayer',
 
@@ -20,11 +22,8 @@ export default {
     },
 
     computed: {
-        videoId () {
-            return this.path.replace(/^.*video\/([^/]+)\/?.*$/, '$1')
-        },
-        embedUrl () {
-            return `https://www.tiktok.com/embed/v2/${this.videoId}?lang=ja`
+        movie () {
+            return new TikTokMovie(this.path)
         }
     },
 }
