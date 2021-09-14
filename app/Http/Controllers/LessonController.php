ac5bdf65a38342919d54760b29a05501e2b9a536
kawamoto <bahihzss@gmail.com>
2021-09-14 21:46:12 +0900

    LessonControllerにマスタデータの取得処理を追加

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -4,40 +4,59 @@ namespace App\Http\Controllers;
 
 use App\Models\Lesson;
 use App\Repositories\Lesson\LessonRepositoryInterface;
+use App\Repositories\MstCategory\MstCategoryRepositoryInterface;
+use App\Repositories\MstCountry\MstCountryRepositoryInterface;
+use App\Repositories\MstLanguage\MstLanguageRepositoryInterface;
 use Illuminate\Http\Request;
 
 class LessonController extends Controller
 {
     private LessonRepositoryInterface $lessonRepository;
+    private MstCategoryRepositoryInterface $mstCategoryRepository;
+    private MstLanguageRepositoryInterface $mstLanguageRepository;
+    private MstCountryRepositoryInterface $mstCountryRepository;
 
     /**
      * LessonController constructor.
-     * @param LessonRepositoryInterface $lessonRepository
+     * @param  LessonRepositoryInterface  $lessonRepository
+     * @param  \App\Repositories\MstCategory\MstCategoryRepositoryInterface  $mstCategoryRepository
+     * @param  \App\Repositories\MstLanguage\MstLanguageRepositoryInterface  $mstLanguageRepository
+     * @param  \App\Repositories\MstCountry\MstCountryRepositoryInterface  $mstCountryRepository
      */
     public function __construct(
-        LessonRepositoryInterface $lessonRepository
+        LessonRepositoryInterface $lessonRepository,
+        MstCategoryRepositoryInterface $mstCategoryRepository,
+        MstLanguageRepositoryInterface $mstLanguageRepository,
+        MstCountryRepositoryInterface $mstCountryRepository
     ) {
         $this->lessonRepository = $lessonRepository;
+        $this->mstCategoryRepository = $mstCategoryRepository;
+        $this->mstLanguageRepository = $mstLanguageRepository;
+        $this->mstCountryRepository = $mstCountryRepository;
     }
 
     /**
      * レッスン検索
      *
-     * @param Request $request
+     * @param  Request  $request
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index(Request $request)
     {
+        $categories = $this->mstCategoryRepository->all();
+        $languages = $this->mstLanguageRepository->all();
+        $countries = $this->mstCountryRepository->all();
+
         // TODO: 検索: Repositoryに検索用のメソッド (ex: getByCondition($searchParam) など) を作成して呼び出す
         $lessons = $this->lessonRepository->getPaginate();
 
-        return view('lessons.index', compact('lessons'));
+        return view('lessons.index', compact('categories', 'languages', 'countries', 'lessons'));
     }
 
     /**
      * レッスン詳細
      *
-     * @param Lesson $lesson
+     * @param  Lesson  $lesson
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function show(Lesson $lesson)
