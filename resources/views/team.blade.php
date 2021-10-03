aff528e7b3247885eec75905f3239b281b63f556
tsubono <tsubono@wss.systems>
2021-10-03 20:43:26 +0900

    team画面作成

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -1,16 +1,34 @@
 @extends('layouts.app')
 
-@section('title', 'TEAM INITについて')
+@section('title', 'TEAM INIT')
 
 @section('content')
     <section class="l-content-block p-team">
         <div class="container pt-40px pb-100px">
-            <div class="border rounded p-box-with-icon">
-                <div class="p-box-with-icon__icon">
-                    <img src="{{ asset('img/mood-bad.svg') }}" alt="エラー">
+            <div class="border rounded member-list">
+                <div class="member-list__item">
+                    <img src="{{ asset('img/team/koshi.png') }}" alt="koshi" class="team-icon">
+                    <p class="team-name">CEO Koshi</p>
                 </div>
-                <div>
-                    <div class="fs-5 fw-bold">ただいま準備中です</div>
+                <div class="member-list__item">
+                    <img src="{{ asset('img/team/Atsushi.png') }}" alt="Atsushi" class="team-icon">
+                    <p class="team-name">CSO Atsuhi</p>
+                </div>
+                <div class="member-list__item">
+                    <img src="{{ asset('img/team/Natsuko.png') }}" alt="Natsuko" class="team-icon">
+                    <p class="team-name">CMO Natsuko</p>
+                </div>
+                <div class="member-list__item">
+                    <img src="{{ asset('img/team/maki.png') }}" alt="maki" class="team-icon">
+                    <p class="team-name">CFO Maki</p>
+                </div>
+                <div class="member-list__item">
+                    <img src="{{ asset('img/team/jun.png') }}" alt="jun" class="team-icon">
+                    <p class="team-name">SA Jun</p>
+                </div>
+                <div class="member-list__item">
+                    <img src="{{ asset('img/team/seiko.png') }}" alt="seiko" class="team-icon">
+                    <p class="team-name">SA Seiko</p>
                 </div>
             </div>
             <div class="mt-5 w-75 mx-auto">
